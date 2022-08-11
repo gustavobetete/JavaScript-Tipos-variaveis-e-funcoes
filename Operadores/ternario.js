4153187fb4ec851b391b353bf67fad7895fdf606
Guuh betete <gustavobetete@hotmail.com>
2022-08-11 20:13:19 -0300

    Operador ternário

---
+++ Operadores/ternario.js
@@ -0,0 +1,18 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+// if normal
+
+if (idadeCliente >= idadeMinima) {
+    console.log("Cerveja");
+} else {
+    console.log("Suco");
+}
+
+// Ternario
+
+            // condição                    // true    //false
+console.log(idadeCliente >= idadeMinima ? "Cerveja" : "suco");
+
+
+// Respostas: suco
