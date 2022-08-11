5496d385da89d75874ba53e46f7b1ad6eda43dad
Guuh betete <gustavobetete@hotmail.com>
2022-08-11 19:37:07 -0300

    Operador ternário

---
+++ Operadores/comparacoes.js
@@ -0,0 +1,13 @@
+// == (Comparação implícita)
+
+const numero = 10;
+const texto = "10";
+
+console.log(numero == texto); // true 
+
+// === (Comparação explícita)
+
+console.log(numero === texto); // false 
+
+console.log(typeof numero); // number
+console.log(typeof texto); // string
