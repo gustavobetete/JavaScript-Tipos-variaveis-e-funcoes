f873be377a50033e55f70dc17a5c9243bbda7c7c
Guuh betete <gustavobetete@hotmail.com>
2022-08-11 20:25:21 -0300

    Template literal/String

---
+++ Tipos primitivos/template-strings.js
@@ -0,0 +1,13 @@
+const nome = "Gu";
+const idade = 2021-1981;
+const cidadeNascimento = "Mogi Mirim";
+
+const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeNascimento;
+
+console.log(apresentacao);
+
+// template string
+
+const apresentacaoTemplate = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`;
+
+console.log(apresentacaoTemplate);
