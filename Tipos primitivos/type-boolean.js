7e3f18029d3a0763815581516fdd47f7b7993987
Guuh betete <gustavobetete@hotmail.com>
2022-08-11 13:05:29 -0300

    Type-Boolean

---
+++ Tipos primitivos/type-boolean.js
@@ -0,0 +1,16 @@
+// Boolean
+
+const primeiroNumero = 10;
+const segundoNumero = 10;
+
+console.log(primeiroNumero === segundoNumero); // true
+
+// como comparar textos
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2); // false
+
+
+
