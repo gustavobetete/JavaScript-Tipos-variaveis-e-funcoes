85686ecac817b4e31c08b2f516a55dda37e19732
Guuh betete <gustavobetete@hotmail.com>
2022-08-11 10:21:42 -0300

    Type Number

---
+++ Tipo number/type-number.js
@@ -0,0 +1,19 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacaoMatematica = numeroPontoFlutuante / pontoFlutuanteSemZero;
+
+console.log(novaOperacaoMatematica);
